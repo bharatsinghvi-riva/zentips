b742696c7a210fb863e033def6b64727fabb6e96
Bharat Singhvi <bharat@riva.co>
2016-08-24 04:44:00 +0530

    Remove sendAs stanza. Add user token variable

--- src/main/java/MessagingService.java
+++ src/main/java/MessagingService.java
@@ -1,15 +1,24 @@
 import co.flock.www.FlockApiClient;
 import co.flock.www.model.messages.FlockMessage;
 import co.flock.www.model.messages.Message;
-import co.flock.www.model.messages.SendAs;
 
 public class MessagingService {
 
     private static final String BOT_TOKEN = "53691730-d1e1-4f46-aa52-e6a6569bee44";
+    private static final String USER_TOKEN = "92b93fcb-965e-4767-a0c9-d720d735f0c1";
 
-    private static void sendMessage(String token, Message message) {
-        SendAs sendAs = new SendAs("Zen Bot", "https://f.flock.co/a49cd4b1471976484521433a");
-        message.setSendAs(sendAs);
+    public static void sendMessage(Message message) {
+        FlockApiClient flockApiClient = new FlockApiClient(USER_TOKEN, false);
+        FlockMessage flockMessage = new FlockMessage(message);
+        try {
+            String response = flockApiClient.chatSendMessage(flockMessage);
+            System.out.println("Send message response:" + response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendMessage(String token, Message message) {
         FlockApiClient flockApiClient = new FlockApiClient(token, false);
         FlockMessage flockMessage = new FlockMessage(message);
         try {
