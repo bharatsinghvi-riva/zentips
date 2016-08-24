370f12baa03165bd5f4bc6479182ac10cb6687fb
Bharat Singhvi <bharat@riva.co>
2016-08-24 10:44:58 +0530

    Add functionality to handle attachment button actions

--- src/main/java/ZenServer.java
+++ src/main/java/ZenServer.java
@@ -1,5 +1,7 @@
 import co.flock.www.model.messages.Message;
+import model.Followers;
 import model.Subscriptions;
+import model.User;
 import model.ZenTip;
 import org.json.JSONObject;
 import spark.ModelAndView;
@@ -35,11 +37,14 @@ public class ZenServer {
 
         post("/", (req, res) -> {
             JSONObject jsonObject = new JSONObject(req.body());
+            System.out.println(req.body());
             String type = (String) jsonObject.get("name");
             if (type.equalsIgnoreCase("app.install")) {
                 handleAppInstall(jsonObject);
             } else if (type.equalsIgnoreCase("client.slashCommand")) {
                 handleSlashCommand(jsonObject);
+            } else if (type.equalsIgnoreCase("client.pressButton")) {
+                handleAttachmentButton(jsonObject);
             }
             res.status(200);
             return "";
@@ -52,6 +57,8 @@ public class ZenServer {
         String userToken = jsonObject.getString("userToken");
         System.out.println("Userid: " + userId);
         System.out.println("userToken: " + userToken);
+        User user = new User(userId, userToken);
+        ZentipAPI.addUser(user);
     }
 
     private static void handleSlashCommand(JSONObject jsonObject) {
@@ -62,9 +69,30 @@ public class ZenServer {
         } else if (text.startsWith("subscribe")) {
             new Subscriptions().addSubscription(text.substring("subscribe".length()), userId);
         } else if (text.startsWith("subscriptions")) {
-            MessagingService.sendMessage(new Message(userId, new Subscriptions().getSubscriptionsForUser(userId).toString()));
+            MessagingService.sendMessage(ZentipAPI.getUserToken(userId), new Message(userId, new Subscriptions().getSubscriptionsForUser(userId).toString()));
         } else if (text.startsWith("unsubscribe")) {
-            MessagingService.sendMessage(new Message(userId, "Unsubscribe successful"));
+            MessagingService.sendMessage(ZentipAPI.getUserToken(userId), new Message(userId, "Unsubscribe successful"));
+        }
+    }
+
+    private static void handleAttachmentButton(JSONObject jsonObject) {
+        String buttonId = jsonObject.getString("buttonId");
+        String userId = jsonObject.getString("userId");
+        if (buttonId.startsWith("follow")) {
+            String split[] = buttonId.split("-");
+            new Followers().addFollower(split[1], userId);
+        } else if (buttonId.startsWith("unfollow")) {
+            String split[] = buttonId.split("-");
+            new Followers().removeFollower(split[1], userId);
+        } else if (buttonId.startsWith("subscribe")) {
+            String split[] = buttonId.split("-");
+            new Subscriptions().addSubscription(split[1], userId);
+        } else if (buttonId.startsWith("unsubscribe")) {
+            String split[] = buttonId.split("-");
+            new Subscriptions().removeSubscription(split[1], userId);
+        } else if (buttonId.startsWith("appreciate")) {
+            String split[] = buttonId.split("-");
+            MessagingService.sendMessage(ZentipAPI.getUserToken(userId), new Message(split[1], "Awesome article!"));
         }
     }
 
@@ -74,9 +102,16 @@ public class ZenServer {
         String message = jsonObject.getString("message");
         String from = jsonObject.getString("from");
         String to = jsonObject.getString("to");
-        MessagingService.sendMessage(new Message(from, "test"));
         ZenTip zenTip = new ZenTip(tag, image, message, from, to);
         ZentipAPI.createMessage(zenTip);
+        for (String userId: new Subscriptions().getSubscribersForTag(tag)) {
+            zenTip = new ZenTip(tag, image, message, from, userId);
+            ZentipAPI.createMessage(zenTip);
+        }
+        for (String userId: new Followers().getFollowers(from)) {
+            zenTip = new ZenTip(tag, image, message, from, userId);
+            ZentipAPI.createMessage(zenTip);
+        }
     }
 
 }
