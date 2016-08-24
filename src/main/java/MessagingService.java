c7b55f82715de6bb57f9e6ad38917cef5f0340b8
Bharat Singhvi <bharat@riva.co>
2016-08-24 10:42:38 +0530

    Remove unnecessary method

--- src/main/java/MessagingService.java
+++ src/main/java/MessagingService.java
@@ -4,19 +4,7 @@ import co.flock.www.model.messages.Message;
 
 public class MessagingService {
 
-    private static final String BOT_TOKEN = "53691730-d1e1-4f46-aa52-e6a6569bee44";
-    private static final String USER_TOKEN = "92b93fcb-965e-4767-a0c9-d720d735f0c1";
-
-    public static void sendMessage(Message message) {
-        FlockApiClient flockApiClient = new FlockApiClient(USER_TOKEN, false);
-        FlockMessage flockMessage = new FlockMessage(message);
-        try {
-            String response = flockApiClient.chatSendMessage(flockMessage);
-            System.out.println("Send message response:" + response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String USER_TOKEN = "81af130a-59bd-4a5e-b112-70166fefcaea";
 
     public static void sendMessage(String token, Message message) {
         FlockApiClient flockApiClient = new FlockApiClient(token, false);
